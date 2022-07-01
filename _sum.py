e238f3cbff65d627ea311291f724a1ce00f26caa
Sadidul Islam <sadidul.012@gmail.com>
2022-07-01 17:11:23 +0600

    Added liquidity score

--- _sum.py
+++ _sum.py
@@ -2,3 +2,7 @@
 
 def _sum(a, b):
     return a + b
+
+
+def liquidity_score(a):
+    return a / 100
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-
+from _sum import _sum, liquidity_score
 
 if __name__ == '__main__':
     print("running main")
+    print("the sum is ", _sum(20, 8))
+    print("liquidity score", liquidity_score(30))
